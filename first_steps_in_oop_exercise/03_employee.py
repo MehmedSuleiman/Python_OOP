a5059a2696cb6b295d5aa8eb0dd3a8c2f3d257e3
Mehmed Suleiman <mehmedsuleiman141@gmail.com>
2026-02-20 18:05:08 +0200

    exercise 3

---
+++ first_steps_in_oop_exercise/03_employee.py
@@ -0,0 +1,22 @@
+class Employee:
+    def __init__(self, id: int, first_name: str, second_name: str, salary: int) -> None:
+        self.id = id
+        self.first_name = first_name
+        self.last_name = second_name
+        self.salary = salary
+
+    def get_full_name(self) -> str :
+        return f"{self.first_name} {self.last_name}"
+
+    def raise_salary(self, num_increase: int) -> int:
+        self.salary += num_increase
+        return self.salary
+
+    def get_annual_salary(self) -> int:
+        return self.salary* 12
+
+
+employee = Employee(744423129, "John", "Smith", 1000)
+print(employee.get_full_name())
+print(employee.raise_salary(500))
+print(employee.get_annual_salary())
