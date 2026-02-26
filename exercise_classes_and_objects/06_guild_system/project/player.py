1b27d54aae3f25dbf0bccaac8d658cac88599899
Mehmed Suleiman <mehmedsuleiman141@gmail.com>
2026-02-26 17:45:56 +0200

    exercise 6

---
+++ exercise_classes_and_objects/06_guild_system/project/player.py
@@ -0,0 +1,22 @@
+class Player:
+    NO_GUILD = "Unaffiliated"
+
+    def __init__(self, name:str, hp:int, mp:int) -> None:
+        self.mp = mp
+        self.hp = hp
+        self.name = name
+        self.skills :dict = {}
+        self.guild = self.NO_GUILD
+
+    def add_skill(self, skill_name: str, mana_cost:int) -> str:
+        if skill_name in self.skills:
+            return "Skill already added"
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self) -> str:
+        result = [f"Name: {self.name}", f"Guild: {self.guild}",f"HP: {self.hp}", f"MP: {self.mp}"]
+        for skill_name, mana_cost in self.skills.items():
+            result.append(f"==={skill_name} - {mana_cost}")
+
+        return "\n".join(result)
